b18706cbb84d945fa3fc3e722e46d5b9357e2c9e
MdRizwan09 <mdrizwanshaikh99@gmail.com>
2025-06-25 15:46:39 +0530

    Debug mode ON and display image in website.

--- mac/settings.py
+++ mac/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-3qcr)!$4ae9bfdpo@m4biif85&=#2y*058gpu6b%$-8fymajzm"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
